468e6448d3bf29b4628c8f8fd9bd058700b9997c
DarrenKwonDev <anatomy1545@gmail.com>
2021-11-01 03:31:59 +0900

    vscode settings

--- src/components/GridWrapper/index.js
+++ src/components/GridWrapper/index.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function GridWrapper({ children }) {
-  return <div>{children}</div>
+  return <GridWrapperRoot>{children}</GridWrapperRoot>
 }
+
+const GridWrapperRoot = styled.div`
+  padding: 0 2rem;
+  ${props => props.theme.deviceSizes.desktop} {
+    padding: 0 8.5rem;
+  }
+`
 
 export default GridWrapper
